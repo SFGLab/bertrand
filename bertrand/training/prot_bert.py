64406a5f86c594af28908bec678953c55ffc14f2
yivlad <jacenko.vlad@gmail.com>
2023-05-25 00:59:52 +0200

    Add flatten layer to prot_bert wrapper

--- bertrand/training/prot_bert.py
+++ bertrand/training/prot_bert.py
@@ -6,10 +6,11 @@ class ProteinClassifier(nn.Module):
     def __init__(self):
         super(ProteinClassifier, self).__init__()
         self.bert = BertForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL_NAME)
-        self.classifier = nn.Sequential(nn.Dropout(p=0.2),
-                                        nn.Linear(self.bert.config.hidden_size, 1),
+        self.classifier = nn.Sequential(nn.Flatten(),
+                                        nn.Dropout(p=0.2),
+                                        nn.Linear(64, 1),
                                         nn.Tanh())
         
     def forward(self, *args, **kwargs):
         output = self.bert(*args, **kwargs)
-        return self.classifier(output.pooler_output)
+        return self.classifier(output.logits)
