cb3d3bda18b6aaf91dc758d0e4f949430df391ed
Alexander Myronov <alexander.myronov@gmail.com>
2023-06-21 10:12:21 +0000

    pre-training

--- train_and_evaluate_nomlm.sh
+++ train_and_evaluate_nomlm.sh
@@ -1,13 +1,12 @@
 set -x
 DATA_DIR=$1
-MODEL_DIR=$2
-OUT_DIR=$3
+#MODEL_DIR=$2
+OUT_DIR=$2
 
 mkdir -p "$OUT_DIR"
 
 python -m bertrand.training.train \
   --input-dir=$DATA_DIR \
-  --model-ckpt=$MODEL_DIR \
   --output-dir=$OUT_DIR \
   --n-splits=21
 
