409c256ba2d7ed1766108aac548256cd8f19c25c
yivlad <jacenko.vlad@gmail.com>
2023-09-09 22:25:50 +0200

    Fix pretraining

--- bertrand-job.sh
+++ bertrand-job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL          # Powiadomienia mailowe. Opcje: NONE, BEGIN, END, FAIL, ALL
 #SBATCH --mail-user=jacenko.vlad@gmail.com     # adres e-mail
-#SBATCH --ntasks=4                    # Uruchomienie na jednym procesorze
-#SBATCH --mem=32gb
-#SBATCH --gpus=a100:1
+#SBATCH --ntasks=16                  
+#SBATCH --mem=128gb
+#SBATCH --gpus=a100:2
 #SBATCH --time=72:00:00               # maksymalny limit czasu DD-HH:MM:SS
 #SBATCH --partition=long
 
@@ -12,6 +12,6 @@ pwd; hostname; date
 source /home2/sfglab/yvladyslav/anaconda3/etc/profile.d/conda.sh
 cd bertrand
 conda activate bertrand
-./analysis.sh "/home2/sfglab/yvladyslav/distilbert/bertrand_results" 4
+./analysis.sh "/home2/sfglab/yvladyslav/distilbert/bertrand_results" 16
  
 date
