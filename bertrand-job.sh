8eb8ea713c1d45013756da1d9e24745376e93967
yivlad <jacenko.vlad@gmail.com>
2023-09-28 22:30:27 +0200

    slurm script absolute paths

--- bertrand-job.sh
+++ bertrand-job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL          # Powiadomienia mailowe. Opcje: NONE, BEGIN, END, FAIL, ALL
 #SBATCH --mail-user=jacenko.vlad@gmail.com     # adres e-mail
-#SBATCH --ntasks=8               
-#SBATCH --mem=64gb
-#SBATCH --gpus=a100:1
+#SBATCH --ntasks=16             
+#SBATCH --mem=128gb
+#SBATCH --gpus=a100:2
 #SBATCH --time=3-00:00:00               # maksymalny limit czasu DD-HH:MM:SS
 #SBATCH --partition=long
 
 pwd; hostname; date
 
 source /home2/sfglab/yvladyslav/anaconda3/etc/profile.d/conda.sh
-cd bertrand
+cd /home2/sfglab/yvladyslav/protbert/bertrand
 conda activate bertrand
 ./analysis.sh "/home2/sfglab/yvladyslav/protbert/bertrand_results" 16
  
